13d3c73e5731667cef7e6aa6f94b14070a385e72
eddy-mb <62819123+emb-develop@users.noreply.github.com>
2023-10-10 09:40:07 -0400

    desarrollo de pagina Home

---
+++ src/components/home/socialNetworks.jsx
@@ -0,0 +1,8 @@
+import { FaGithub, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa6";
+
+export const SocialNetworks = [
+  { name: <FaLinkedin size={32} />, url: "" },
+  { name: <FaTwitter size={32} />, url: "" },
+  { name: <FaYoutube size={32} />, url: "" },
+  { name: <FaGithub size={32} />, url: "" },
+];
---
+++ src/components/nav-bar/navBar.jsx
@@ -0,0 +1,38 @@
+"use client";
+import Image from "next/image";
+import perfil from "@/img/perfil.png";
+import Link from "next/link";
+import { LinkNav } from "./url";
+
+export default function NavBar() {
+  return (
+    <nav className="  flex text-white justify-between h-20 w-full fixed">
+      <div className=" flex w-full m-2.5 items-center">
+        <Image
+          className="mx-2.5"
+          src={perfil}
+          width={50}
+          height={50}
+          alt="Logotipo"
+        />
+        <h2 className="mx-2.5 font-medium">EDDY MAMANI</h2>
+      </div>
+      <div className="h-full">
+        <ul className="flex items-center h-full pr-20">
+          {LinkNav.map(({ text, url }) => {
+            return (
+              <li className=" mx-6 font-medium">
+                <Link
+                  className="hover:text-violet-600 p-2"
+                  key={url}
+                  href={url}>
+                  {text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
